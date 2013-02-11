34430c084015b5baf65f71ba5c4b3348cc63d639
pi <ncvc@mit.edu>
2013-02-11 04:55:19 +0000

    moar blacklist

---
+++ thechillbar/SignAnimator.py
@@ -0,0 +1,90 @@
+import SocketServer
+import Queue
+import threading
+import os.path
+import socket
+import time
+import sys
+
+sys.path.append('/home/pi/')
+from alphasign import AlphaSign
+
+SOCKET_NAME = "/tmp/thechillsignsocket"
+StreamServer = None
+
+try:
+    StreamServer = SocketServer.UnixStreamServer
+except AttributeError:
+    # Probably on a Windows machine
+    StreamServer = SocketServer.BaseServer
+
+class SignAnimator(StreamServer, object):
+    def __init__(self, handler):
+        super(SignAnimator, self).__init__(SOCKET_NAME, handler)
+        self.queue = Queue.Queue(1)
+        self.message_pusher = MessagePusher(self.queue)
+        self.message_pusher.start()
+
+    def shutdown(self):
+        self.message_pusher.join()
+        super(Animator, self).shutdown()
+
+class SignAnimationRequestHandler(SocketServer.StreamRequestHandler):
+    def handle(self):
+        try:
+            self.server.queue.put(self.rfile.readline().strip(), False)
+        except Queue.Full:
+            pass
+
+class MessagePusher(threading.Thread, object):
+    def __init__(self, queue, poll_time=1.0):
+        super(MessagePusher, self).__init__()
+        self.stop_request = threading.Event()
+        self.queue        = queue
+        self.poll_time    = poll_time
+        self.sign         = AlphaSign.Sign('/dev/ttyUSB0')
+        self.sign.setClock()
+
+    def run(self):
+        print 'Message pusher thread started'
+        while not self.stop_request.isSet():
+            try:
+                next_message = self.queue.get(False)
+                self.sign.sendText('A', AlphaSign.encodeText(next_message), AlphaSign.MODE_ROTATE)
+            except Queue.Empty:
+                pass
+            time.sleep(self.poll_time)
+            
+    def join(self, timeout=None):
+        print 'Message Pusher thread exiting'
+        self.stop_request.set()
+        super(MessagePusher, self).join(timeout)
+
+def sendMessage(message):
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    try:
+        sock.connect(SOCKET_NAME)
+        sock.send(message)
+    except IOError as e:
+        if e.errno == 13:
+            print "{0}\nTry again using sudo".format(e)
+        elif e.errno == 111:
+            print "{0}\nError connecting to socket {1}\nMake sure the server is running and is on this socket".format(e, SOCKET_NAME)
+        else:
+            print e
+    except Exception as e:
+        print e
+    finally:
+        sock.close()
+
+if __name__ == "__main__":
+
+    print "Creating sign animator server"
+    if os.path.exists(SOCKET_NAME):
+        os.remove(SOCKET_NAME)
+
+    server = SignAnimator(SignAnimationRequestHandler)
+    print "Starting sign animation server..."
+    print "Sign Animation server is running on socket {0}".format(SOCKET_NAME)
+    print "Quit the server with CONTROL-C."
+    server.serve_forever()
--- thechillbar/views.py
+++ thechillbar/views.py
@@ -5,57 +5,70 @@ from django.views.decorators.csrf import csrf_exempt
 from django.template import RequestContext
 import settings
 import simplejson
+import SignAnimator
 
 # Hack to get lights working from here
 import sys
 sys.path.append('/home/pi/')
 from bar_lighting import Strip, Animator
 
-from alphasign import AlphaSign
-
 NUM_PIXELS = 32
+UGC_LENGTH_LIMIT = 300
 
 @csrf_exempt
 def home(request):
-    #blacklist filtering
-    resp = checkIP(request)
-    if resp != None:
-    	return resp
+    blacklisted = checkIP(request)
 
     #GET requests return main page
-    if request.method == "GET":
-        return render_to_response('index.html', context_instance=RequestContext(request))
+    if request.method == "GET" and not blacklisted:
+        return filter('index.html', request, blacklisted)
     
     #POST requests process posted data and perform LED actions
     if request.method == "POST":
-        Animator.sendMessage(request.POST.get('command'))
-        return render_to_response('index.html', context_instance=RequestContext(request))
-
+        command = request.POST.get('command')[:UGC_LENGTH_LIMIT]
+        print getIP(request), command
+        if not blacklisted:                         
+            Animator.sendMessage(command)
+        return filter('index.html', request, blacklisted)
+        
 @csrf_exempt
 def sign(request):
-    if request.method == "GET":
-        return render_to_response('index.html', context_instance=RequestContext(request))
+    blacklisted = checkIP(request)
     
-    elif request.method == "POST":
-        s = AlphaSign.Sign('/dev/ttyUSB0')
-        s.setClock()
-        s.sendText('A', AlphaSign.encodeText(request.POST.get('message')), AlphaSign.MODE_ROTATE)
-        return render_to_response('sign.html', context_instance=RequestContext(request))
+    message = request.POST.get('message')[:UGC_LENGTH_LIMIT]
+    print getIP(request), message
+
+    if request.method == "POST" and not blacklisted:
+        SignAnimator.sendMessage(message)
+
+    return filter('index.html', request, blacklisted)
 
 def checkIP(request):
+    ip = getIP(request)
+
+    last_ips = open('past_ips.txt', 'a+')
+    last_ips.write(ip + '\n')
+    last_ips.close()
+
+    f = open('ip_blacklist.txt')
+    
+    if ip in f.read():
+    	f.close()
+        print 'IP %s blocked by blacklist' % ip
+        return True
+    f.close()
+    return False
+
+def getIP(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
+    return ip    
 
-    last_ips = open('past_ips.txt', 'a+')
-    last_ips.write(ip + '\n')
-
-    f = open('ip_blacklist.txt')
-    
-    if ip in f.read():
-    	print 'IP %s blocked by blacklist' % ip
-    	return render_to_response('blacklist.html', context_instance=RequestContext(request))
-    print ip
-    return None
+def filter(filename, request, blacklisted):
+    if blacklisted:
+        return render_to_response('blacklist.html', context_instance=RequestContext(request))
+    else:
+        return render_to_response(filename, context_instance=RequestContext(request))
